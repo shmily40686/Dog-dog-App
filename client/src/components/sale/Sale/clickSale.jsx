4d1e264097695bfdce8b78baf9a8c40d203e1f5e
Frog <shmily40686@gmail.com>
2018-07-19 15:51:40 -0700

    simple css for cilckpage

--- client/src/components/sale/Sale/clickSale.jsx
+++ client/src/components/sale/Sale/clickSale.jsx
@@ -30,7 +30,7 @@ constructor(props) {
   		var obj = {};
   		obj.src = uil;
   		obj.width = 1;
-  		obj.height = 0.7;
+  		obj.height = 0.8;
   		return obj
   	})
   	return photos;
@@ -93,8 +93,10 @@ constructor(props) {
 		return(
 			<div className='margin-left-30'>
 			  <button className='basic-btn' onClick={this.props.changeView}>BACK</button>
+        <div className='post-page-title-box'>
 				<h1 className='post-page-title'>{this.props.currentPost.title}</h1>
-				<div>
+        </div>
+				<div className='photosGallery'>
 					<div>
   	        <Gallery photos={this.state.threePhotos} onClick={this.openLightbox} />
   	      </div>
@@ -106,28 +108,32 @@ constructor(props) {
 	          isOpen={this.state.lightboxIsOpen}
 	        />
 				</div>
-				<div className='post-page-box'>
-					<h3 className='infoTitle'>Info</h3>
-					<div>Type {this.props.currentPost.info.type}</div>
-					<div>Ago {this.props.currentPost.info.age.year} year<span>{this.props.currentPost.info.age.month} month</span></div>
-					<div>Size {this.props.currentPost.info.size}</div>
-					<div>Sex {this.props.currentPost.info.sex}</div>
-					<div>Color {this.props.currentPost.info.color}</div>
-				</div>
-				<div className='post-page-box'>
-					<h3 className='priceTitle' >Price</h3>
-					<div>Full-Price {this.props.currentPost.info.price.fullPrice}</div>
-					<div>Deposit {this.props.currentPost.info.price.deposit}</div>
-				</div>
-				<div className='post-page-box'>
-					<h3 className='connectTitle'>Connect</h3>
-					<div>Email {this.props.currentPost.info.connect.email}</div>
-					<div>Call {this.props.currentPost.info.connect.call}</div>
-				</div>
-				<div className='post-page-box'>
-					<h3 className='locationTitle'>Location</h3>
-					<div>{this.props.currentPost.location.city}<span>{this.props.currentPost.location.state}</span></div>
-				</div>
+        <div className='small-box'>
+  				<div className='info-box '>
+    					<h3 className='infoTitle'>Info</h3>
+    					<div>Type {this.props.currentPost.info.type}</div>
+    					<div>Ago {this.props.currentPost.info.age.year} year<span>{this.props.currentPost.info.age.month} month</span></div>
+    					<div>Size {this.props.currentPost.info.size}</div>
+    					<div>Sex {this.props.currentPost.info.sex}</div>
+    					<div>Color {this.props.currentPost.info.color}</div>
+  				</div>
+  				<div className='price-box'>
+  					<h3 className='priceTitle' >Price</h3>
+  					<div>Full-Price {this.props.currentPost.info.price.fullPrice}</div>
+  					<div>Deposit {this.props.currentPost.info.price.deposit}</div>
+  				</div>
+        </div>
+        <div className='small-box'>
+  				<div className='connect-box'>
+  					<h3 className='connectTitle'>Connect</h3>
+  					<div>Email {this.props.currentPost.info.connect.email}</div>
+  					<div>Call {this.props.currentPost.info.connect.call}</div>
+  				</div>
+  				<div className='location-box'>
+  					<h3 className='locationTitle'>Location</h3>
+  					<div>{this.props.currentPost.location.city}<span>{this.props.currentPost.location.state}</span></div>
+  				</div>
+        </div>
 				<div className='post-page-box'>
 					<h3 className='descriptionTitle'>Description</h3>
 					<div>{this.props.currentPost.description}</div>
@@ -138,16 +144,16 @@ constructor(props) {
             {
   						this.props.currentPost.comments.map((comment,i) => {
   							return(
-  							 <div comment={comment} key={i} >
-  							 	 <div>{comment.user }<span >{moment(comment.creatAt).startOf('hour').fromNow()}</span></div>
-  							 	 <div>{comment.text}</div>
+  							 <div comment={comment} key={i} className='comment-box' >
+  							 	 <div><span className='comment-userName'>{comment.user }</span><span className='comment-time'>{moment(comment.creatAt).startOf('hour').fromNow()}</span></div>
+  							 	 <div className='comment-text'>{comment.text}</div>
   							 	 <button className='basic-btn'>Reply</button>
   							 </div>
   							)
   						})
             }
           </div>
-					<textarea rows="4" cols="50" onChange={this.getText} /><br/>
+					<textarea className='comment-textarea' rows="4" cols="50" onChange={this.getText} /><br/>
 					<button className='basic-btn' onClick={this.addReply} >Add Reply</button>
 				</div>
 			</div>
