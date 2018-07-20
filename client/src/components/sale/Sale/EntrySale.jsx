ee113118023a1ad0fe9dd34e791a1c55aedde38f
Frog <shmily40686@gmail.com>
2018-07-20 16:55:46 -0700

    sale/post/css

--- client/src/components/sale/Sale/EntrySale.jsx
+++ client/src/components/sale/Sale/EntrySale.jsx
@@ -14,42 +14,42 @@ class EntrySale extends React.Component {
 					<img className='post-image' src={this.props.post.photo[0]}/>
 						<div style={{width:'100px'}}>{this.props.post.type}</div>
 						<div className="infoBoxEntrySale">
-							<div className='entry-sale-item age'>
+							<div className='entry-sale-item '>
 								<span className='entry-sale-field-title'>Age:</span>
 								<span className='entry-sale-field-value'>{this.props.post.info.age.year > 0 ? this.props.post.info.age.year > 1 ? `${this.props.post.info.age.year} years` : `${this.props.post.info.age.year} year` : ''}</span>
 								<span className='entry-sale-field-value'>{this.props.post.info.age.month > 0 ? this.props.post.info.age.month > 1 ? `${this.props.post.info.age.month} months` : `${this.props.post.info.age.month} month` : ''}</span>
 							</div>
-							<div className='entry-sale-item location'>
+							<div className='entry-sale-item '>
 								<span className='entry-sale-field-title'>Location:</span>
 								<span className='entry-sale-field-value'>{this.props.post.location.city}</span>
 								<span className='entry-sale-field-value'>{this.props.post.location.state}</span>
 							</div>
-							<div className='entry-sale-item price'>
+							<div className='entry-sale-item '>
 								<span className='entry-sale-field-title'>Price:</span>
 								<span className='entry-sale-field-value'>{this.props.post.info.price.fullPrice}</span>
 							</div>
 						</div>
 						<div className="infoBoxEntrySale">
-							<div className='entry-sale-item sex'>
+							<div className='entry-sale-item '>
 								<span className='entry-sale-field-title'>Sex:</span>
 								<span className='entry-sale-field-value'>{this.props.post.info.sex}</span>
 							</div>
-							<div className='entry-sale-item breed'>
+							<div className='entry-sale-item '>
 								<span className='entry-sale-field-title'>Breed:</span>
 								<span className='entry-sale-field-value'>{this.props.post.info.type}</span>
 							</div>
-							<div className='entry-sale-item size'>
+							<div className='entry-sale-item '>
 								<span className='entry-sale-field-title'>Size:</span>
 								<span className='entry-sale-field-value'>{this.props.post.info.size}</span>
 							</div>
 						</div>
 						<div className="infoBoxEntrySale">
-							<div className='entry-sale-item time'>
+							<div className='entry-sale-item '>
 								<span className='entry-sale-field-title left'>Posted:</span>
 								<span className='entry-sale-field-value left'>{moment(this.props.post.creatAt).startOf('hour').fromNow()}</span>
 							</div>
 							<div className='entry-sale-item'></div>
-							<div className='entry-sale-item view'>
+							<div className='entry-sale-item '>
 								<span className='entry-sale-field-title'>Views:</span>
 								<span className='entry-sale-field-value'>{this.props.post.view}</span>
 							</div>
